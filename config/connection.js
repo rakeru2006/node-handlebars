7176bce28f70c3e795fec45744bf74344797c9a0
rakeru2006 <rakeru2006@gmail.com>
2021-01-16 14:49:51 -0600

    changin to enviroment  variable

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,11 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -9,6 +13,7 @@ var connection = mysql.createConnection({
   database: "cat_db"
 });
 
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
